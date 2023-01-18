021430cfd9c48c2f1a20c8a8bdc143c61227eb05
Anatolii Bespalko <tolyan9901@gmail.com>
2023-01-18 17:13:49 +0200

    some fix

--- src/main/java/chatApi/storage/Storage.java
+++ src/main/java/chatApi/storage/Storage.java
@@ -7,10 +7,10 @@ import java.util.List;
 
 public class Storage {
 
-    public static List<User> activeUserList = new ArrayList<>(); // 5user
+    public static List<User> activeUserList = new ArrayList<>();
 
     public static void removeBySession(String session){
-        for (int i=0;i<activeUserList.size();i++){ // i=  0  1  2  3  4
+        for (int i=0;i<activeUserList.size();i++){
             if(activeUserList.get(i).getSession().equals(session)){
                 activeUserList.remove(i);
                 break;
